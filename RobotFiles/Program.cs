9106ae992594dea828e217a3055df14a059f4698
hobby203 <edward24hobbs@outlook.com>
2015-03-16 12:27:11 +0000

    cleanup & more variables

--- RobotFiles/Program.cs
+++ RobotFiles/Program.cs
@@ -7,34 +7,36 @@ public class Program{
         try {
             Console.WriteLine("Hey hey");
             Console.ReadKey();
-            var brain = new Brick<Sensor, Sensor, Sensor, Sensor>("com6"); //set up brick
+            var brain = new Brick<Sensor, Sensor, Sensor, Sensor>("com6"); //set up brick here to allow all functions access
             brain.Connection.Open(); //connect to brick
 
-            //brain.Sensor1 = new Sensor();
-            //brain.Sensor1.ReadAsString();
+            //brain.Sensor2 = new Sensor();
+            //brain.Sensor2.ReadAsString(); //idk if this'll work, test it
+
+            brain.Sensor3 = new ColorSensor();
+            brain.Sensor1 = new TouchSensor();
 
             brain.Vehicle.LeftPort = MotorPort.OutA;    //
             brain.Vehicle.RightPort = MotorPort.OutD;   // initialise 'vehicle' motors
             brain.Vehicle.ReverseLeft = false;          //
             brain.Vehicle.ReverseRight = true;          //
+            sbyte speed = 20;                           // robot's speed, will need changing
 
             ConsoleKeyInfo quitKey;                         //
             Console.WriteLine("Press Q to exit program");   // set up exit clause
 
             do
             {
-
-            } while (quitKey != ConsoleKey.Q);
+                quitKey = Console.ReadKey(true);
+                bool mode;
+                Thread swapMode = new Thread (checkButton); // (checkButton(brain,mode));
+                
+            } while (quitKey.Key != ConsoleKey.Q);
         }
         catch(Exception e) {
             Console.WriteLine("Error: "+ e.Message);
             Console.WriteLine("Press any key ...");
             Console.ReadKey();
         }
-    }
-
-    static void initialise()
-    {
-        
-    }
+    }      
 }
