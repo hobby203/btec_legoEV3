aea675557ffe8d6a081b11af5997c407c97fbf36
hobby203 <edward24hobbs@outlook.com>
2015-03-16 10:52:22 +0000

    basic skeleton stuff

--- RobotFiles/Program.cs
+++ RobotFiles/Program.cs
@@ -1,15 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using MonoBrick.EV3;
+//using MonoBrick.NXT;
 
-namespace RobotFiles
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+public class Program{
+    static void Main(string[] args){
+        try {
+            Console.WriteLine("Hey hey");
+            Console.ReadKey();
+            var brain = new Brick<Sensor, Sensor, Sensor, Sensor>("com6"); //set up brick
+            brain.Connection.Open(); //connect to brick
+
+            //brain.Sensor1 = new Sensor();
+            //brain.Sensor1.ReadAsString();
+
+            brain.Vehicle.LeftPort = MotorPort.OutA;    //
+            brain.Vehicle.RightPort = MotorPort.OutD;   // initialise 'vehicle' motors
+            brain.Vehicle.ReverseLeft = false;          //
+            brain.Vehicle.ReverseRight = true;          //
+
+            ConsoleKeyInfo quitKey;                         //
+            Console.WriteLine("Press Q to exit program");   // set up exit clause
+
+            do
+            {
+
+            } while (quitKey != ConsoleKey.Q);
         }
+        catch(Exception e) {
+            Console.WriteLine("Error: "+ e.Message);
+            Console.WriteLine("Press any key ...");
+            Console.ReadKey();
+        }
+    }
+
+    static void initialise()
+    {
+        
     }
 }
