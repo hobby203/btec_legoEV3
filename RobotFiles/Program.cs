0a7ea5125afa645260897d8bfb4d72ed4e8c18ca
hobby203 <edward24hobbs@outlook.com>
2015-03-27 15:16:40 +0000

    Tidy up code, add comments

--- RobotFiles/Program.cs
+++ RobotFiles/Program.cs
@@ -4,83 +4,58 @@ using MonoBrick.EV3;
 
 public class Program{
     static void Main(string[] args){
-        var brain = new Brick<Sensor, Sensor, Sensor, Sensor>("com4"); //set up brick here to allow all functions access
+        var brain = new Brick<Sensor, Sensor, Sensor, Sensor>("com4"); //set up brick information
         try {
 
             brain.Connection.Open(); //connect to brick
 
             brain.Sensor2 = new UltrasonicSensor(UltrasonicMode.Centimeter); // set ultrasonic sensor mode
             brain.Sensor3 = new ColorSensor(ColorMode.Color); // set color sensor mode
-            //brain.Sensor3 = new TouchSensor(TouchMode.Boolean); // set touch sensor to button mode
 
             brain.Vehicle.LeftPort = MotorPort.OutA;    //
             brain.Vehicle.RightPort = MotorPort.OutD;   // initialise 'vehicle' motors
-            brain.Vehicle.ReverseLeft = false;          //
-            brain.Vehicle.ReverseRight = false;          //
+            brain.Vehicle.ReverseLeft = false;
+            brain.Vehicle.ReverseRight = false;
 
-            sbyte speed = 127;                          // robot's speed, will need changing
-            sbyte turn_percent = 90;
-            string surfaceColor = "Black";                  //colour for robot to stay on
-            int maxDistance = 20;                      // distance where 'collision' becomes possible
+            sbyte speed = 127;                  // robot's speed, will need changing
+            sbyte turn_percent = 90;            // how much the robot will turn if it hits something
+            string surfaceColor = "Black";      // colour for robot to stay on
+            int maxDistance = 20;               // distance where 'collision' becomes possible
 
             Console.WriteLine("Press any key to exit program");   // set up exit clause
 
-            Random randomVal = new Random();    //random number for when i need it
-            while (!Console.KeyAvailable)
+            Random randomVal = new Random();    // random number for when i need it
+            while (!Console.KeyAvailable)       // allows program to exit if you press a button
             {
-                bool edgeMode = false; //check if edge-sensing mode enabled
-                Console.WriteLine(brain.Sensor3.ReadAsString());
-                Console.WriteLine(brain.Sensor2.ReadAsString());
-                //Console.WriteLine(brain.Sensor1.ReadAsString());
-                if (brain.Sensor3.ReadAsString() == "1")
-                { //enable/disable edge-sensing mode depending on previous state
-                    Console.WriteLine("woooah the button was pressed");
-                    if (edgeMode == false)
-                    {
-                        edgeMode = true;
-                    }
-                    else
-                    {
-                        edgeMode = false;
-                    }
-                }
-                //if robot leaves surface, could be dodgy if it goes at an angle
-                // I sincerely apologise for this god-awful conditional here
-                string curDistance_string = brain.Sensor2.ReadAsString();
-                curDistance_string = curDistance_string.Remove(2);
-                int curDistance = int.Parse(curDistance_string);
-                if (brain.Sensor3.ReadAsString() != surfaceColor | curDistance <= maxDistance)
+                string curDistance_string = brain.Sensor2.ReadAsString();   // read current distance
+                curDistance_string = curDistance_string.Remove(2);          // remove the 'cm' from the end
+                int curDistance = int.Parse(curDistance_string);            // convert to an integer, for comparison
+                if (brain.Sensor3.ReadAsString() != surfaceColor | curDistance <= maxDistance) // check if  object is getting too close
                 {
-                    Console.WriteLine("saw something");
-                    brain.Vehicle.Brake();
-                    if (randomVal.Next(-1, 2) == 1) //pick a random direction
+                    brain.Vehicle.Brake();  // stop the motors, allows vehicle to reverse
+                    if (randomVal.Next(-1, 2) == 1) // pick a random direction to turn
                     {
-                        brain.Vehicle.TurnLeftReverse(speed, turn_percent);
-                        Console.WriteLine("spun left");
+                        brain.Vehicle.TurnLeftReverse(speed, turn_percent); // move backwards whilst turning left
                     }
                     else
                     {
-                        brain.Vehicle.TurnRightReverse(speed, turn_percent);
-                        Console.WriteLine("spun right");
+                        brain.Vehicle.TurnRightReverse(speed, turn_percent); // move backwards whilst turning right
                     } 
                 }
                 else
                 {
-                    brain.Vehicle.Forward(speed);
+                    brain.Vehicle.Forward(speed); // move forwards
                 }
-                
-
-                //Console.WriteLine("got to the end!");
             }
         }
-        catch(Exception e) {
+        catch(Exception e) { // catch errors from connecting to brick
             Console.WriteLine("Error: "+ e.Message);
             Console.WriteLine("Press any key ...");
             Console.ReadKey();
         }
-        finally
+        finally // once button has been pressed
         {
-            brain.Vehicle.Brake();
+            brain.Vehicle.Brake(); // stop the vehicle motors
             brain.Connection.Close(); //close connection
         }
     }      
